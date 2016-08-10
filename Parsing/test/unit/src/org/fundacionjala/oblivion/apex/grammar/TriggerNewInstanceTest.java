2269bb33c7c9842968b9f48123d26d4a18ff3799
Amir Aranibar <amir.aranibar@gmail.com>
2016-08-10 11:59:36 -0400

    Add unit tests for the property 'new'

---
+++ Parsing/test/unit/src/org/fundacionjala/oblivion/apex/grammar/TriggerNewInstanceTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) Fundacion Jala. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+package org.fundacionjala.oblivion.apex.grammar;
+
+import java.io.FileNotFoundException;
+import org.fundacionjala.oblivion.apex.grammar.jcclexer.ApexParser;
+import org.fundacionjala.oblivion.apex.grammar.jcclexer.ParseException;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Amir Aranibar
+ */
+public class TriggerNewInstanceTest extends AbstractTestGrammar {
+
+    private final static String RESOURCE_FOLDER = "resources/trigger/%s";
+
+    @Test
+    public void callNewPropertyOfTrigger() throws FileNotFoundException, ParseException {
+        ApexParser parser = getParser("callNewPropertyOfTrigger.cls");
+        parser.CompilationUnit();
+        assertTrue(NO_PARSER_ERRORS_ARE_EXPECTED, parser.getSyntaxErrors().isEmpty());
+    }
+
+    @Test
+    public void callNewPropertyOfTriggerAndCallMethodInline() throws FileNotFoundException, ParseException {
+        ApexParser parser = getParser("callNewPropertyOfTriggerAndCallMethodInline.cls");
+        parser.CompilationUnit();
+        assertTrue(NO_PARSER_ERRORS_ARE_EXPECTED, parser.getSyntaxErrors().isEmpty());
+    }
+
+    @Test
+    public void callNewPropertyOfCustomClass() throws FileNotFoundException, ParseException {
+        ApexParser parser = getParser("callNewPropertyOfCustomClass.cls");
+        parser.CompilationUnit();
+        assertEquals(PARSER_ERRORS_ARE_EXPECTED, 1, parser.getSyntaxErrors().size());
+    }
+
+    @Override
+    protected String getResourceFolder() {
+        return RESOURCE_FOLDER;
+    }
+}
